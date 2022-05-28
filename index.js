0edb253cd8929e4ccc3b297bd2b7b87486d389d4
Derrick <derrickryan10@gmail.com>
2022-05-28 09:05:31 -0400

    Add display functions

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+// const btn1 = document.getElementById('btn1');
+// const btn2 = document.getElementById('btn2');
+// const btn3 = document.getElementById('btn3');
+// const btn4 = document.getElementById('btn4');
+// const btn5 = document.getElementById('btn5');
+// const btn6 = document.getElementById('btn6');
+// const btn7 = document.getElementById('btn7');
+// const btn8 = document.getElementById('btn8');
+// const btn9 = document.getElementById('btn9');
+// const btn0 = document.getElementById('btn0');
+
+// const addBtn = document.getElementById('add');
+// const subBtn = document.getElementById('subtract');
+// const mulButn = document.getElementById('multiply');
+// const divBtn = document.getElementById('divide');
+
+// const eqBtn = document.getElementById('equals');
+
+// const clrBtn = document.getElementById('clear');
+// const delBtn = document.getElementById('delete');
+
+// const decBtn = document.getElementById('decimal');
+// const negBtn = document.getElementById('negative');
+// const pctBtn = document.getElementById('percentage');
+
+let displayBox = "";
+
+for(let i = 0; i < 10; i++){
+    document.getElementById('btn'+i).addEventListener('click', display);
+}
+
+document.getElementById('add').addEventListener('click', display);
+document.getElementById('subtract').addEventListener('click', display);
+document.getElementById('multiply').addEventListener('click', display);
+document.getElementById('divide').addEventListener('click', display);
+
+document.getElementById('clear').addEventListener('click', clear);
+document.getElementById('delete').addEventListener('click', del);
+
+function display(e){
+    displayBox += e.target.textContent;
+    document.getElementById('display').textContent = displayBox;
+}
+
+function clear(){
+    displayBox = "";
+    document.getElementById('display').textContent = displayBox;
+}
+
+function del(){
+    if(displayBox != ""){
+        displayBox = displayBox.slice(0, -1);
+        document.getElementById('display').textContent = displayBox;
+    }
+}
+
 function add(a, b){
     return a + b;
 }
